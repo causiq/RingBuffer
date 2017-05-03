90935c1fd445695b0c12ec91776d0c14e03bee65
$+j <lust4life.jun@gmail.com>
2017-05-03 13:11:15 +0800

    use array + two unmasked indices implement ringbuffer
      - ref: https://www.snellman.net/blog/archive/2016-12-13-ring-buffers/
      - add more tests

--- RingBuffer.fsx
+++ RingBuffer.fsx
@@ -7,15 +7,72 @@
 #load "RingBuffer.fs"
 open Hopac
 open Expecto
+open Hopac.Infixes
 
 let tests =
   testList "RingBuffer" [
     testCaseAsync "enqueue" (job {
-      let! rb = RingBuffer.create 2u
+      let! rb = RingBuffer.create 4us
+
+      let mutable result = []
+      
+      let takePerSecond = Job.delay <| fun () ->
+        timeOutMillis 100 ^=>. job {
+          let! res = RingBuffer.take rb
+          result <- ( res :: result)
+        }
+      Job.foreverServer takePerSecond |> run
+      
       do! RingBuffer.put rb "a"
-      let! res = RingBuffer.take rb
-      Expect.equal res "a" "Got value"
+      do! RingBuffer.put rb "b"
+      do! RingBuffer.put rb "c"
+      do! RingBuffer.put rb "d"
+      do! RingBuffer.put rb "e"
+      do! RingBuffer.put rb "f"
+      do! RingBuffer.put rb "g"
+
+      do! timeOutMillis 1000 ^=>. job {
+            Expect.equal result (List.rev ["a";"b";"c";"d";"e";"f";"g"]) "Got value"
+          }
     } |> Job.toAsync)
+
+    testCaseAsync "take all/batch" (job {
+      let! rb = RingBuffer.create 4us
+      
+      do! RingBuffer.put rb "a"
+      do! RingBuffer.put rb "b"
+      do! RingBuffer.put rb "c"
+      do! RingBuffer.put rb "d"
+      let! res = RingBuffer.takeBatch 3us rb
+      Expect.equal res ([|"a";"b";"c"|]) "Got value"
+      do! RingBuffer.put rb "e"
+      do! RingBuffer.put rb "f"
+      do! RingBuffer.put rb "g"
+
+      let! res = RingBuffer.takeAll rb
+      Expect.equal res ([|"d";"e";"f";"g";|]) "Got value"
+
+    } |> Job.toAsync)
+
+    testList "ringSizeValidate" [
+      test "0 is not allowed" {
+        Expect.equal (Utils.ringSizeValidate 0us) false "(ringSizeValidate 0)"
+      }
+
+      test "1 is allowed" {
+        Expect.equal (Utils.ringSizeValidate 1us) true "(ringSizeValidate 1)"
+      }
+
+      test "is power of 2" {
+        Expect.equal (Utils.ringSizeValidate 8us) true "(ringSizeValidate 8)"
+      }
+
+      test "half the range of the index data types (uint16)" {
+        Expect.equal (Utils.ringSizeValidate (uint16 (System.Math.Pow(2.,15.)))) true "(ringSizeValidate pow 2 15)"
+      }
+    ] 
+    
   ]
+
 
 Tests.runTests defaultConfig tests
